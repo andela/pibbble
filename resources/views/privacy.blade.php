538894fdfe2b9ee4b078867b7d92f6869c6eb206
andela-sakande <surajudeen.akande@andela.com>
2015-09-20 12:40:01 +0000

    [Pibbble][#103319506] Design of about,privacy and terms pages

---
+++ resources/views/privacy.blade.php
@@ -0,0 +1,70 @@
+@extends('master')
+@section('title', 'Privacy')
+
+@section('content')
+    <div class="container-fluid all">
+      <div class="container">
+         <div class="panel panel-info">
+            <div class="panel-heading">
+              <h1 class="panel-title">Privacy Policy</h1>
+            </div>
+            <div class="panel-body">
+              <h2 class="section">Updated September 19, 2015</h2>
+              <p>This Privacy Policy governs the manner in which Pibbble collects, uses, maintains and discloses information collected from users (each, a "User") of the http://www.pibbble.com website ("Site").</p>
+              <h2 class="section">Personal identification information</h2>
+              <p> We may collect personal identification information from Users in a variety of ways, including, but not limited to, when Users visit our site, register on the site, fill out a form, and in connection with other activities, services, features or resources we make available on our Site.Users may be asked for, as appropriate, name, email address. Users may, however, visit our Site anonymously. We will collect personal identification information from Users only if they voluntarily submit such information to us. Users can always refuse to supply personally identification information, except that it may prevent them from engaging in certain Site related activities.</p>
+              <h2 class="section">Non-personal identification information</h2>
+              <p> We may collect non-personal identification information about Users whenever they interact with our Site. Non-personal identification information may include the browser name, the type of computer and technical information about Users means of connection to our Site, such as the operating system and the Internet service providers utilized and other similar information.</p>
+              <h2 class="section">Web browser cookies</h2>
+              <p> Our Site may use "cookies" to enhance User experience. User's web browser places cookies on their hard drive for record-keeping purposes and sometimes to track information about them. User may choose to set their web browser to refuse cookies, or to alert you when cookies are being sent. If they do so, note that some parts of the Site may not function properly.</p>
+              <h2 class="section">How we use collected information</h2>
+              <p> Pibbble may collect and use Users personal information for the following purposes: </p>
+              <ul>
+                <li>
+                  <i>To run and operate our Site</i><br/>
+                  We may need your information display content on the Site correctly.
+                </li>
+                <li>
+                  <i>To improve customer service</i><br/>
+                  Information you provide helps us respond to your customer service requests and support needs more efficiently.
+                </li>
+                <li>
+                  <i>To personalize user experience</i><br/>
+                  We may use information in the aggregate to understand how our Users as a group use the services and resources provided on our Site.
+                </li>
+                <li>
+                  <i>To improve our Site</i><br/>
+                  We may use feedback you provide to improve our products and services.
+                </li>
+                <li>
+                  <i>To run a promotion, contest, survey or other Site feature</i><br/>
+                  To send Users information they agreed to receive about topics we think will be of interest to them.
+                </li>
+                <li>
+                  <i>To send periodic emails</i><br/>
+                  We may use the email address to send User information and updates pertaining to their order. It may also be used to respond to their inquiries, questions, and/or other requests.
+                </li>
+              </ul>
+
+              <h2 class="section">How we protect your information</h2>
+              <p> We adopt appropriate data collection, storage and processing practices and security measures to protect against unauthorized access, alteration, disclosure or destruction of your personal information, username, password, transaction information and data stored on our Site. </p>
+
+              <h2 class="section">Sharing your personal information</h2>
+              <p> We do not sell, trade, or rent Users personal identification information to others. We may share generic aggregated demographic information not linked to any personal identification information regarding visitors and users with our business partners, trusted affiliates and advertisers for the purposes outlined above. </p>
+
+              <h2 class="section">Third party websites</h2>
+              <p> We adopt appropriate data collection, storage and processing practices and security measures to protect against unauthorized access, alteration, disclosure or destruction of your personal information, username, password, transaction information and data stored on our Site. </p>
+
+              <h2 class="section">Changes to this privacy policy</h2>
+              <p> Pibbble has the discretion to update this privacy policy at any time. When we do, we will post a notification on the main page of our Site. We encourage Users to frequently check this page for any changes to stay informed about how we are helping to protect the personal information we collect. You acknowledge and agree that it is your responsibility to review this privacy policy periodically and become aware of modifications.</p>
+
+              <h2 class="section">Your acceptance of these terms</h2>
+              <p> By using this Site, you signify your acceptance of this policy. If you do not agree to this policy, please do not use our Site. Your continued use of the Site following the posting of changes to this policy will be deemed your acceptance of those changes. This policy was generated using <a href="http://privacypolicies.com" target="_blank">PrivacyPolicies.Com</a> </p>
+
+              <h2 class="section">Contacting us</h2>
+              <p> If you have any questions about this Privacy Policy, the practices of this site, or your dealings with this site, please contact us. </p>
+            </div>
+        </div>
+      </div>
+    </div>
+@endsection
