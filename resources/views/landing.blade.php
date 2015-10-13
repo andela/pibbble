1bc1db0ab2e432543a92332ba89cec9af917683f
andela-womokoro <wilson.omokoro@andela.com>
2015-10-13 10:18:07 +0100

    [Pibbble][#102991688] Seperate PHP code from landing view

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -2,7 +2,6 @@
 @section('title', 'Pibbble')
 
 @section('content')
-</nav>
   <nav class="navbar navbar-default" style="background-color: #fff; margin-top:-20px;">
     <div id="lower-navbar">
         <ul class="nav navbar-nav navbar-left" style="padding-left:15px; color: #777;">
@@ -14,39 +13,26 @@
   </nav>
 
   <div class="container">
-    <?php
-    for ($i = 0; $i < 12; $i++) {
-      echo("<div class='projects-container'>");
-      echo("<div class='projects'>");
-      echo("<a href=''>");
-      echo("<img src='/img/shot.png' width='200' height='150' style='border:0px solid #ccc;' />");
-      echo("</a>");
-      echo("<span class='project-stats'><i class='fa fa-thumbs-o-up'></i>&nbsp;0</span>");
-      echo("<span class='project-stats'><i class='fa fa-eye'></i>&nbsp;0</span>");
-      echo("</div>");
-      echo("<span class='projects-name'>Project title</span>");
-      echo("</div>");
-    }
-    ?>
+    @include('others.temp_grid')
     <br clear="left" />
     <center>
-    <ul class="pagination">
-      <li>
-        <a href="#" aria-label="Previous">
-          <span aria-hidden="true">&laquo;</span>
-        </a>
-      </li>
-      <li><a href="#">1</a></li>
-      <li><a href="#">2</a></li>
-      <li><a href="#">3</a></li>
-      <li><a href="#">4</a></li>
-      <li><a href="#">5</a></li>
-      <li>
-        <a href="#" aria-label="Next">
-          <span aria-hidden="true">&raquo;</span>
-        </a>
-      </li>
-    </ul>
+        <ul class="pagination">
+          <li>
+            <a href="#" aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+            </a>
+          </li>
+          <li><a href="#">1</a></li>
+          <li><a href="#">2</a></li>
+          <li><a href="#">3</a></li>
+          <li><a href="#">4</a></li>
+          <li><a href="#">5</a></li>
+          <li>
+            <a href="#" aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+            </a>
+          </li>
+        </ul>
     </center>
   </div>
 @endsection
---
+++ resources/views/others/temp_grid.blade.php
@@ -0,0 +1,14 @@
+<?php
+  for ($i = 0; $i < 12; $i++) {
+    echo("<div class='projects-container'>");
+    echo("<div class='projects'>");
+    echo("<a href=''>");
+    echo("<img src='/img/shot.png' width='200' height='150' style='border:0px solid #ccc;' />");
+    echo("</a>");
+    echo("<span class='project-stats'><i class='fa fa-thumbs-o-up'></i>&nbsp;0</span>");
+    echo("<span class='project-stats'><i class='fa fa-eye'></i>&nbsp;0</span>");
+    echo("</div>");
+    echo("<span class='projects-name'>Project title</span>");
+    echo("</div>");
+  }
+?>
