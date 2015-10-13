27d6561943185d952ee193f206dd6acd22ab5565
andela-womokoro <wilson.omokoro@andela.com>
2015-10-13 11:19:17 +0100

    [Pibbble][#102991688] Apply blade syntax to temp_grid.blade.php

--- resources/views/others/temp_grid.blade.php
+++ resources/views/others/temp_grid.blade.php
@@ -1,14 +1,10 @@
-<?php
-  for ($i = 0; $i < 12; $i++) {
-    echo("<div class='projects-container'>");
-    echo("<div class='projects'>");
-    echo("<a href=''>");
-    echo("<img src='/img/shot.png' width='200' height='150' style='border:0px solid #ccc;' />");
-    echo("</a>");
-    echo("<span class='project-stats'><i class='fa fa-thumbs-o-up'></i>&nbsp;0</span>");
-    echo("<span class='project-stats'><i class='fa fa-eye'></i>&nbsp;0</span>");
-    echo("</div>");
-    echo("<span class='projects-name'>Project title</span>");
-    echo("</div>");
-  }
-?>
+@for ( $i = 0; $i < 12; $i++ )
+    <div class='projects-container'>
+    <div class='projects'>
+    <a href=''><img src='/img/shot.png' width='200' height='150' style='border:0px solid #ccc;' /></a>
+    <span class='project-stats'><i class='fa fa-thumbs-o-up'></i>&nbsp;0</span>
+    <span class='project-stats'><i class='fa fa-eye'></i>&nbsp;0</span>
+    </div>
+    <span class='projects-name'>Project title</span>
+    </div>
+@endfor
