79c2eff14850bdc28177538fb7cfaf5cf30cfaeb
Ogunjimi Opeyemi Abiodun <opeyemi.ogunjimi@andela.com>
2015-10-16 10:26:14 -0400

    Apply fixes from StyleCI

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,6 +22,6 @@ $factory->define(Pibbble\User::class, function (Faker\Generator $faker) {
         'bio' => $faker->realText($maxNbChars = 200, $indexSize = 2),
         'location' => $faker->address,
         'avatar' => $faker->url,
-        'remember_token' => str_random(10)
+        'remember_token' => str_random(10),
     ];
 });
