1ced052cb9daed7a11bad43bc3c7a27aa835c9f3
Wilson Omokoro <wilson.omokoro@andela.com>
2015-10-26 16:22:16 +0100

    [Pibble][#102991688] Modify migration and seeders

--- resources/views/others/temp_grid.blade.php
+++ resources/views/others/temp_grid.blade.php
@@ -1,7 +1,7 @@
 @foreach ($projects as $project)
     <div class='projects-container'>
     <div class='projects'>
-    <a href=''><img src='/img/shot.png' width='200' height='150' style='border:0px solid #ccc;' /></a>
+    <a href=''><img src='{{ $project->url }}' width='200' height='150' style='border:0px solid #ccc;' /></a>
     <span class='project-stats'><i class='fa fa-thumbs-o-up'></i>&nbsp;{{ $project->likes }}</span>
     <span class='project-stats'><i class='fa fa-eye'></i>&nbsp;{{ $project->views }}</span>
     </div>
