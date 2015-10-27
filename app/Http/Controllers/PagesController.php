bb1382046bb277fc4e7cae6d1276785a860fa1cd
Wilson Omokoro <wilson.omokoro@andela.com>
2015-10-27 12:52:23 +0100

    [Pibble][#102991688] Include actual functional pagination on landing page

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -11,7 +11,7 @@ class PagesController extends Controller
      */
     public function home()
     {
-        $projects = Project::all();
+        $projects = Project::paginate(12);
 
         return view('landing', ['projects' => $projects]);
     }
