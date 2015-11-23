b1fa17f95c3164ad879e78f894d341de16f59eaa
andela-oogunjimi <opeyemi.ogunjimi@andela.com>
2015-11-23 13:44:06 +0000

    Update the AuthController

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -29,7 +29,7 @@
               <input type="password" class="mui-form-control" name="password_confirmation" id="password_confirmation" placeholder="confirm password" required>
             </div>
             <div align="left" class="mui-form-group">
-              <input type="submit" class="mui-btn" value="Sign up">
+              <input type="submit" class="mui-btn" value="register">
             </div>
              <div align="left" class="mui-form-group">
                 <p><span class="valign">Or sign up with:</span>
