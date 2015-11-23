d77369b5c8a487af40ae367e7160e63880fbba6c
andela-oogunjimi <opeyemi.ogunjimi@andela.com>
2015-11-23 10:10:06 +0000

    add email validation to registration process

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,6 +7,11 @@ use Pibbble\Project;
 
 class PagesController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('validateEmail', ['only' => ['home']]);
+    }
+
     /**
      * @return landing.blade.php
      */
