01434d3884ef1cf122d20d6fdad44d4c57e6899a
andela-sakande <surajudeen.akande@andela.com>
2015-11-23 13:14:41 +0100

    [Pibbble][#138] Fix dashboard project modals

--- resources/views/projects/dashboard.blade.php
+++ resources/views/projects/dashboard.blade.php
@@ -93,13 +93,39 @@
                                 @foreach ($projects as $project)
                                 <div class='projects-container'>
                                     <div class='projects'>
-                                        <a href=''><img src="{{ $project->url }}" width='200' height='150' style='border:0px solid #ccc;'>
+                                        <a href="" data-toggle="modal" data-target="#{{ $project->id }}"><img src="{{ $project->url }}" width='200' height='150' style='border:0px solid #ccc;'>
                                         </a>
                                         <span class='project-stats'><i class='fa fa-thumbs-o-up'></i>&nbsp;{{ $project->likes }}</span>
                                         <span class='project-stats'><i class='fa fa-eye'></i>&nbsp;{{ $project->views }}</span>
                                     </div>
                                     <span class='projects-name'><a href="">{{ $project->projectname }}</a></span>
                                     </div>
+                                    <div class="modal fade-lg" id="{{ $project->id }}" role="dialog">
+                                  <div class="modal-dialog modal-lg">
+                                    <div class="modal-content">
+                                      <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                        <br />
+                                        <h3 class="modal-title">{{ $project->projectname }}</h3>
+                                        <p>by</p> <a href="">{{ $project->user->username }}</a>
+                                      </div>
+                                      <div class="modal-body">
+                                        <div class="modal-left">
+                                            <img src='{{ $project->url }}' width="600" height="400" />
+                                            <div class="modal-right">
+                                                <p><i class='fa fa-thumbs-o-up'></i>&nbsp;{{ $project->views }}&nbsp;likes</p>
+                                                <p><i class='fa fa-eye'></i>&nbsp;{{ $project->views }}&nbsp;views</p>
+                                            </div>
+                                        </div>
+                                        <br clear="left">
+                                        <p>{{ $project->description }}</p>
+                                      </div>
+                                      <div class="modal-footer">
+                                        <button type="button" class="btn btn-info" data-dismiss="modal">Close</button>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
                                 @endforeach
                             </div>
                         @endif
