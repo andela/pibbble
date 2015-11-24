b665bca08a9e463213cc359b872298354611bc01
Ogunjimi Opeyemi Abiodun <opeyemi.ogunjimi@andela.com>
2015-11-24 12:36:10 -0500

    Applied fixes from StyleCI

--- app/Http/Middleware/ValidateEmail.php
+++ app/Http/Middleware/ValidateEmail.php
@@ -17,10 +17,8 @@ class ValidateEmail
      */
     public function handle($request, Closure $next)
     {
-        if ($request->has('_token') && $request->session()->has('_token'))
-        {
-            if ($request->_token === $request->session()->pull('_token')) 
-            {
+        if ($request->has('_token') && $request->session()->has('_token')) {
+            if ($request->_token === $request->session()->pull('_token')) {
                 Auth::login(User::create($request->session()->all()));
             }
         }
