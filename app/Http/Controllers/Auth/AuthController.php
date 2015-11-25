586da053a38e9bbaedc8034c06df54505a6c97b0
andela-oogunjimi <opeyemi.ogunjimi@andela.com>
2015-11-25 08:51:04 +0000

    [Pibbble][#108779138] Add email verification to the registration process

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -222,17 +222,12 @@ class AuthController extends Controller
 
             $m->to($request->email, $request->username)->subject('Confirm your email address.');
         });
-        /*
-        $request->session()->put('_token', $request->_token);
-        $request->session()->put('username', $request->username);
-        $request->session()->put('email', $request->email);
-        $request->session()->put('password', bcrypt($request->password));
-        */
+        
         $request->session(['_token' => $request->_token,
             'username' => $request->username,
             'email' => $request->email,
             'password' => bcrypt($request->password),
-            ]);
+        ]);
 
         return view('/auth/confirmemail');
     }
