3cf8c44780905aedcf388a9f50ef75ab0c7930d9
andela-oogunjimi <opeyemi.ogunjimi@andela.com>
2015-11-25 10:04:13 +0000

    [Pibbble][#108779138] Refactor

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -222,12 +222,7 @@ class AuthController extends Controller
 
             $m->to($request->email, $request->username)->subject('Confirm your email address.');
         });
-        /*
-        $request->session()->put('_token', $request->_token);
-        $request->session()->put('username', $request->username);
-        $request->session()->put('email', $request->email);
-        $request->session()->put('password', bcrypt($request->password));
-        */
+        
         $request->session(['_token' => $request->_token,
             'username' => $request->username,
             'email' => $request->email,
