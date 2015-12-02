19213bb8a8878fdc80c51d26214b90aa06c4b255
andela-oogunjimi <opeyemi.ogunjimi@andela.com>
2015-12-02 17:42:17 +0000

    [Pibbble][#108779138] Implement design change
    Redirect the user to the login page after email verification

--- app/Http/Middleware/ValidateEmail.php
+++ app/Http/Middleware/ValidateEmail.php
@@ -17,8 +17,9 @@ class ValidateEmail
      */
     public function handle($request, Closure $next)
     {
-        if ($request->_token === $request->session()->pull('_token')) {
-            Auth::login(User::create($request->session()->all()));
+        if ($request->_token === $request->session()->get('_token')) {
+            User::create($request->session()->all());
+            return redirect('/auth/login');
         }
 
         return $next($request);
