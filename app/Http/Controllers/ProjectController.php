bc71a8ea7c9b9f08d1157ea7011e4b207887858a
andela-cganga <ganga.chris@gmail.com>
2015-12-09 12:36:22 +0300

    [Pibbble][#109696954] Removed empty function edit from ProjectController

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -98,16 +98,6 @@ class ProjectController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
