635fddea5c1681c0c9d986ee5884ba6a248e57fd
andela-cganga <ganga.chris@gmail.com>
2015-12-09 12:35:55 +0300

    Pibbble][#109696954] removed empty function show from ProjectController

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -98,17 +98,6 @@ class ProjectController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
