d552d785d994a22412d3ba7745d2329da8a4c953
Wilson Omokoro <wilson.omokoro@andela.com>
2015-12-15 11:58:54 +0100

    [Pibbble][#106189306] Comment  ProjectLikes Controller

--- app/Http/Controllers/ProjectLikesController.php
+++ app/Http/Controllers/ProjectLikesController.php
@@ -9,6 +9,12 @@ use Pibbble\ProjectLikes;
 
 class ProjectLikesController extends Controller
 {
+    /**
+     *  If a user "likes" a project, this method inserts a record representing the like into the database.
+     *  The like is deleted if the user later "unlikes" the project.
+     * @param  $projectID       [the id of the project liked]
+     * @return response         [JSON response returned to the AJAX calling function]
+     */
     public function like($projectID)
     {
         $userID = Auth::user()->id;
