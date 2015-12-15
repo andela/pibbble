c8547a4aa463cd4e3a4beaace254c89cb20c66fa
Ogunjimi Opeyemi Abiodun <opeyemi.ogunjimi@andela.com>
2015-12-15 16:33:44 +0100

    Update and rename 2015_12_15_140000_rename_projectUrl_to_project_url.php to 2015_12_15_140000_add_project_url_column.php

--- database/migrations/2015_12_15_140000_add_project_url_column.php
+++ database/migrations/2015_12_15_140000_add_project_url_column.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class RenameProjectUrlToProjectUrl extends Migration
+class AddProjectUrlColumn extends Migration
 {
     /**
     * Run the migrations.
@@ -13,7 +13,7 @@ class RenameProjectUrlToProjectUrl extends Migration
     public function up()
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->renameColumn('projectUrl', 'project_url');
+            $table->string('project_url')->nullable()->unique();
         });
     }
 
@@ -25,7 +25,7 @@ class RenameProjectUrlToProjectUrl extends Migration
     public function down()
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->renameColumn('project_url', 'projectUrl');
+            $table->dropColumn('project_url');
         });
     }
 }
