bc806001d5cca4d91047c54d15631e4f9ef35528
andela-cganga <ganga.chris@gmail.com>
2016-01-06 18:05:37 +0300

    (fix) Add token to twitter request

--- tests/OAuthTest.php
+++ tests/OAuthTest.php
@@ -15,8 +15,12 @@ class OAuthTest extends TestCase
 
     public function testTwitterOAuth()
     {
-        $response = $this->call('GET', '/auth/twitter');
-        var_dump($response);
+        Session::start(); // Start a session for the current test
+        $params = [
+            '_token' => csrf_token(), // Retrieve current csrf token
+        ];
+
+        $response = $this->call('GET', '/auth/twitter', $params);
 
         $this->assertEquals(302, $response->getStatusCode());
 
