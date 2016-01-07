117d7508681fa4a8249fafc629cb3602c4be87b1
Wilson Omokoro <wilson.omokoro@andela.com>
2016-01-07 08:59:37 +0100

    [Fixes #106189306] Remove redundant lines

--- app/Http/Controllers/ProjectViewsController.php
+++ app/Http/Controllers/ProjectViewsController.php
@@ -5,19 +5,15 @@ namespace Pibbble\Http\Controllers;
 use DB;
 use Auth;
 use Pibbble\Project;
-use Pibbble\ProjectLikes;
 
 class ProjectViewsController extends Controller
 {
-
     public function view($projectID)
     {
         $project = Project::find($projectID);
         $project->views += 1;
         $project->save();
 
-        $views = Project::find($projectID)->views;
-
-        return response()->json($views);
+        return response()->json($project->views);
     }
 }
