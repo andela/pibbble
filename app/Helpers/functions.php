7b97297cc64c525016fda98243fa9183f9b65fa0
andela-cganga <ganga.chris@gmail.com>
2016-01-12 12:25:23 +0300

    [Fixes #111395814] Add helper function sanitize for social media handles

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -6,3 +6,15 @@ if (! function_exists('load_asset')) {
         return (env('APP_ENV') === 'PRODUCTION') ? secure_asset($asset_url) : asset($asset_url);
     }
 }
+
+if (! function_exists('sanitize')) {
+    /**
+     * Check if user fields are empty
+     *
+     * @param $social_handle
+     * $return boolean
+     */
+    function sanitize($social_handle) {
+        return (! empty($social_handle) && ! ctype_space($social_handle) ? true : false);
+    }
+}
