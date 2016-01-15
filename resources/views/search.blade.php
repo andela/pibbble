826c4a60dca8a4e4c7d0d712ddb357c9b178a0fe
andela-sakande <surajudeen.akande@andela.com>
2016-01-15 10:04:31 +0000

    (fix) Change url to image_url

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -18,7 +18,7 @@
         <div class='projects-container'>
           <div class='projects'>
               <!-- Trigger modal window when a project thumbnail is clicked -->
-              <a data-toggle="modal" data-target="#{{ $project->id }}"><img src='{{ $project->url }}' width='200' height='150' /></a>
+              <a data-toggle="modal" data-target="#{{ $project->id }}"><img src='{{ $project->image_url }}' width='200' height='150' /></a>
               <span class='project-stats'><i class='fa fa-thumbs-o-up'></i>&nbsp;{{ $project->likes }}</span>
               <span class='project-stats'><i class='fa fa-eye'></i>&nbsp;{{ $project->views }}</span>
           </div>
@@ -39,7 +39,7 @@
               </div>
               <div class="modal-body">
                 <div class="modal-left">
-                    <img src='{{ $project->url }}'  width="600" height="400"/>
+                    <img src='{{ $project->image_url }}'  width="600" height="400"/>
                     <div class="modal-right">
                         <p><i class='fa fa-thumbs-o-up'></i>&nbsp;{{ $project->views }}&nbsp;likes</p>
                         <p><i class='fa fa-eye'></i>&nbsp;{{ $project->views }}&nbsp;views</p>
