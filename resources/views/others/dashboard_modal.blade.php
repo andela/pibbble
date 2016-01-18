17c643016a1982a4cd2c295d57d3c3d80bfbb17f
andela-cganga <ganga.chris@gmail.com>
2016-01-18 18:38:06 +0300

    [Feature #111395680] Add Follow modal

--- resources/views/others/dashboard_modal.blade.php
+++ resources/views/others/dashboard_modal.blade.php
@@ -189,3 +189,23 @@
 
     </div>
 </div>
+
+<!-- Skills Modal -->
+<div class="modal fade" id="followModal" role="dialog">
+    <div class="modal-dialog modal-sm">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Either Following/Followers</h4>
+            </div>
+            <div class="modal-body">
+                Body Goes Here
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
