7cfc9fb7e37ae9390120460836c386818636870a
andela-cganga <ganga.chris@gmail.com>
2016-01-19 10:28:49 +0300

    [Feature #111395680] Add modals for followings and followers

--- resources/views/others/dashboard_modal.blade.php
+++ resources/views/others/dashboard_modal.blade.php
@@ -190,15 +190,34 @@
     </div>
 </div>
 
-<!-- Skills Modal -->
-<div class="modal fade" id="followModal" role="dialog">
+<!-- Followers Modal -->
+<div class="modal fade" id="followersModal" role="dialog">
     <div class="modal-dialog modal-sm">
 
         <!-- Modal content-->
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Either Following/Followers</h4>
+                <h4 class="modal-title">Followers</h4>
+            </div>
+            <div class="modal-body">
+                Body Goes Here
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="followingModal" role="dialog">
+    <div class="modal-dialog modal-sm">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Following</h4>
             </div>
             <div class="modal-body">
                 Body Goes Here
--- resources/views/projects/dashboard.blade.php
+++ resources/views/projects/dashboard.blade.php
@@ -84,9 +84,9 @@ Dashboard
                             <h4>Activity Feed</h4>
                         </div>
                         <div class="panel-body">
-                            <a>Following <span class="badge pull-right">{{ $user->following }}</span></a>
+                            <a href="#" data-toggle="modal" data-target="#followingModal">Following <span class="badge pull-right">{{ $user->following }}</span></a>
                             <hr>
-                            <a>Followers <span class="badge pull-right" id="followersSpan">{{ $user->followers }}</span></a>
+                            <a href="#" data-toggle="modal" data-target="#followersModal">Followers <span class="badge pull-right" id="followersSpan">{{ $user->followers }}</span></a>
                             <hr>
                             <p>Projects <span class="badge pull-right">{{ $user->projects->count() }}</span></p>
                         </div>
