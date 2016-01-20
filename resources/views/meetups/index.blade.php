685107fb350ab8abfabed4aba5e70a9a04ac1d5a
dipo <oladipupo.isola@andela.com>
2016-01-20 18:47:07 +0100

    (Feature #111860601) Add content to meetup page

--- resources/views/meetups/index.blade.php
+++ resources/views/meetups/index.blade.php
@@ -2,10 +2,15 @@
 @section('title', 'Meetups')
 
 @section('content')
-<div class="" style="min-height: 390px">
-  <div class="cover-bg">
-    <p class="cover-swords">HANG OUT WITH OTHER DEVELOPERS</p>
-    <p class="cover-bwords">Host a Meetup</p>
-  </div>
+<div class="" style="min-height: 420px">
+    <div class="cover-bg">
+        <p class="cover-swords">HANG OUT WITH OTHER DEVELOPERS</p>
+        <p class="cover-bwords">Host a Meetup</p>
+    </div>
+    <div class="meetup-form">
+        <form>
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        </form>
+    </div>
 </div>
 @endsection
