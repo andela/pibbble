6a7b48b209b7debac89a05aca391929132af4d8f
andela-sakande <surajudeen.akande@andela.com>
2016-01-25 09:25:40 +0000

    [Chore] WIP

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -10,9 +10,18 @@
     </div>
     <hr class="ash">
     <div class="row">
-        <div class="col-sm-9">
-            <div class="info">
-                <img src="">
+        <div class="col-md-9">
+            <div class="col-md-2">
+                <img src="http://placehold.it/80x80" class="team-pix">
+            </div>
+            <div class="col-md-4">
+                <h5>Yellow <i class="fa fa-envelope hire"></i></h5>
+                <p>Lagos, Nigeria</p>
+                <small>ui, ux, android, games </small>
+                <br>
+                <i class="fa fa-plus-square fa-lg"></i>
+                <p>450  |  54,201</p>
+                <small> shots  | followers</small>
             </div>
             <div class="shots">
 
