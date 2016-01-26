53e7a82c6af88f02377fc52e1f4967aa7578ee40
dipo <oladipupo.isola@andela.com>
2016-01-26 14:20:46 +0100

    (Feature #111860601) Create index view.

--- resources/views/meetups/index.blade.php
+++ resources/views/meetups/index.blade.php
@@ -5,12 +5,53 @@
 <div class="" style="min-height: 420px">
     <div class="cover-bg">
         <p class="cover-swords">HANG OUT WITH OTHER DEVELOPERS</p>
-        <p class="cover-bwords">Host a Meetup</p>
+        <p class="cover-bwords"><strong>Host a Meetup</strong></p>
     </div>
     <div class="meetup-form">
-        <form>
-            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        </form>
+        <div class="row">
+            <div class="col-md-12 col-sm-12">
+                <div class="form-text">Want to host a Pibbble meetup? Fill the following...</div>
+                <form action="" method="" class="">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <div class="container">
+                        <fieldset class="form-group">
+                            <label class="">City</label>
+                            <div class="">
+                                <input type="text" class="input-field form-control" name="city">
+                            </div>
+                        </fieldset>
+                        <fieldset class="form-group">
+                            <label class="">Date & Time</label>
+                            <div class="">
+                                <input type="text" class="input-field form-control">
+                            </div>
+                        </fieldset>
+                        <fieldset class="form-group">
+                            <label class="">Details</label>
+                            <div class="">
+                                <textarea type="text" class="form-control textarea-field" rows="3" name="event_details"></textarea>
+                            </div>
+                        </fieldset>
+                        <fieldset class="form-group">
+                            <label class="">Organizer mailing address</label>
+                            <div class="">
+                                <textarea type="text" class="form-control textarea-field" rows="3" name="organizer_address"></textarea>
+                            </div>
+                        </fieldset>
+                        <fieldset class="form-group">
+                            <label class="">Phone</label>
+                            <div class="">
+                                <input type="date" class="input-field form-control" name="phone_no">
+                            </div>
+                        </fieldset>
+                        <div class="form-group">
+                            <label class=""></label>
+                            <button type="submit" class="btn profile-button form-group">Submit Meetup</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
