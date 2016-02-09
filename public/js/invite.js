486b0c16578026aebe42f6c884a587d88ad3cd14
andela-cganga <ganga.chris@gmail.com>
2016-02-09 12:28:07 +0300

    [Feature #111601192] Send invite request as ajax

--- public/js/invite.js
+++ public/js/invite.js
@@ -1,5 +1,35 @@
+var input = $('.typeahead');
+var team = input.attr('data-team');
+var invites = [];
 $.getJSON('/teams/invites', function (data) {
-    console.log(data);
-}, function(error) {
-    console.log(error);
+    input.typeahead({
+            source: data,
+            autoSelect: true
+        });
+});
+
+input.change(function() {
+    var current = input.typeahead("getActive");
+    var html = '';
+    html += '<div class="team-item">';
+    html += '<a href="/' + current.name + '">';
+    html += '<img src="' + current.avatar + '">';
+    html += '<div class="team-name">';
+    html += '<b>';
+    html += '<a href="/' + current.name + '">';
+    html += current.name;
+    html += '</a>';
+    html += '</b>';
+    html += '</div>';
+    html += '</div>';
+
+    $('.invitees').append(html);
+
+    input.val('');
+
+    $.getJSON('/teams/' + team + '/invite/' + current.id, function(data) {
+        console.log(data);
+    }, function(error) {
+        console.log(error);
+    });
 });
