e8b015e65bbd63f4d2b4153fd775d8db98802b0b
andela-cganga <ganga.chris@gmail.com>
2016-02-09 20:05:01 +0300

    [Feature #111601192] Designing dashboard view

--- resources/views/teams/dashboard.blade.php
+++ resources/views/teams/dashboard.blade.php
@@ -8,25 +8,40 @@
 
 @section('content')
     <nav class="navbar navbar-default" style="background-color: #fff; margin-top:-20px;">
-    <div class="navbar-content">
-    <div class="pull-left">
-        <ul class="navbar-links">
-            <li><a href="" class="all-teams badge"><span class="name">TEAM</span></a></li>
-            <li><a href=""><span class="count">467  </span><span class="criteria"> Shots</span></a></li>
-            <li><a href=""><span class="count">54,697  </span><span class="criteria"> Followers</span></a></li>
-            <li><a href=""><span class="count">7  </span><span class="criteria"> Members</span></a></li>
-            <li><a href=""  class="dropdown dropdown-toggle" data-toggle="dropdown"><span class="criteria">More </span><span class="caret caret_color"></span></a></li>
-            <ul class="dropdown-menu">
-              <li><a href="#">HTML</a></li>
-              <li><a href="#">CSS</a></li>
-              <li><a href="#">JavaScript</a></li>
-            </ul>
-        </ul>
-    </div>
-    <div class="pull-right">
-        abn
-    </div>
-
-    </div>
+        <div class="navbar-content">
+            <div class="pull-left">
+                <ul class="navbar-links">
+                    <li><a href="" class="all-teams badge"><span class="name">TEAM</span></a></li>
+                    <li><a href=""><span class="count">467  </span><span class="criteria"> Shots</span></a></li>
+                    <li><a href=""><span class="count">54,697  </span><span class="criteria"> Followers</span></a></li>
+                    <li><a href=""><span class="count">7  </span><span class="criteria"> Members</span></a></li>
+                    <li><a href=""  class="dropdown dropdown-toggle" data-toggle="dropdown"><span class="criteria">More </span><span class="caret caret_color"></span></a></li>
+                    <ul class="dropdown-menu">
+                      <li><a href="#">HTML</a></li>
+                      <li><a href="#">CSS</a></li>
+                      <li><a href="#">JavaScript</a></li>
+                    </ul>
+                </ul>
+            </div>
+            <div class="pull-right team-actions">
+                <select name="" id="">
+                    <option value="">Team Shots</option>
+                    <option value="">Personal Shots</option>
+                </select>
+                <a href="#" class="btn btn-primary btn-small hire">Hire Us</a>
+                <button id="followTeam" class="btn btn-primary">Follow</button>
+                <button class="btn"><i class="fa fa-cog"></i></button>
+            </div>
+        </div>
     </nav>
+    <div class="content">
+        <div class="row col-md-12">
+            <img class="img team-pix" src="http://placehold.it/100x100">
+        </div>
+        <div class="row col-md-12">
+            <h2 class="img">Team name</h2>
+            <h4 class="img hire">Nairobi Kenya</h4>
+            <h5 class="img">Lorem Ipsum quote about this awesome team. Will probably limit the number of comments. Let me make this a little longer</h5>
+        </div>
+    </div>
 @endsection
