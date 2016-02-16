febeb83f027ede08d72a03b6351232948886c0b9
andela-cganga <ganga.chris@gmail.com>
2016-02-16 10:52:53 +0300

    [Feature #111601192] View counts for followers, members and projects for teams

--- resources/views/teams/navbar.blade.php
+++ resources/views/teams/navbar.blade.php
@@ -2,16 +2,10 @@
         <div class="navbar-content">
             <div class="pull-left">
                 <ul class="navbar-links">
-                    <li><a href="" class="all-teams badge"><span class="name">TEAM</span></a></li>
-                    <li><a href=""><span class="count">467  </span><span class="criteria"> Shots</span></a></li>
-                    <li><a href=""><span class="count">54,697  </span><span class="criteria"> Followers</span></a></li>
-                    <li><a href=""><span class="count">7  </span><span class="criteria"> Members</span></a></li>
-                    <li><a href=""  class="dropdown dropdown-toggle" data-toggle="dropdown"><span class="criteria">More </span><span class="caret caret_color"></span></a></li>
-                    <ul class="dropdown-menu">
-                      <li><a href="#">HTML</a></li>
-                      <li><a href="#">CSS</a></li>
-                      <li><a href="#">JavaScript</a></li>
-                    </ul>
+                    <li><a href="" class="all-teams badge"><span class="name">{{ $team->name }}</span></a></li>
+                    <li><a href=""><span class="count"> {{ $team->projects()->count() }}  </span><span class="criteria"> Projects</span></a></li>
+                    <li><a href=""><span class="count" id="followersCount">{{ $team->followers()->count() }}  </span><span class="criteria"> Followers</span></a></li>
+                    <li><a href=""><span class="count">{{ $team->members()->count() }}  </span><span class="criteria"> Members</span></a></li>
                 </ul>
             </div>
             <div class="pull-right team-actions">
