634438ffaeac817df9a5debc995b300234b43b63
andela-cganga <ganga.chris@gmail.com>
2016-02-16 18:01:19 +0300

    [Feature #111601192] Confirm delete modal

--- resources/views/teams/dashboard_modal.blade.php
+++ resources/views/teams/dashboard_modal.blade.php
@@ -1,3 +1,23 @@
+<!-- Delete Modal-->
+<!-- Modal -->
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Delete {{ $team->name }}</h4>
+      </div>
+      <div class="modal-body">
+        <p>Are you sure you want to delete <strong> {{ $team->name }}</strong></p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <a href="{{ url("/teams/delete/".$team->id ) }}" type="button" class="btn btn-primary">Delete</a>
+      </div>
+    </div>
+  </div>
+</div>
+
 <!-- Hire Us modal-->
 <div class="modal fade" id="hireTeam" role="dialog">
     <div class="modal-dialog">
