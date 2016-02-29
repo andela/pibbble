b625df3d65fdf25dbdf08f24c082ac7eb4a89453
andela-cganga <ganga.chris@gmail.com>
2016-02-29 15:39:56 +0300

    [Chore #114660529] Test for project views

--- tests/ProjectTest.php
+++ tests/ProjectTest.php
@@ -42,6 +42,7 @@ class ProjectTest extends TestCase
         $user = factory(Pibbble\User::class)->create();
         $project = factory(Pibbble\Project::class)->create();
 
+        // Like a project
         $response = $this->actingAs($user)
                     ->call(
                         'GET',
@@ -49,5 +50,30 @@ class ProjectTest extends TestCase
                     );
         $this->assertEquals(200, $response->status());
         $this->seeInDatabase('projects_likes', ['user_id' => $user->id, 'project_id' => $project->id]);
+
+        // Unlike a project
+        $response = $this->actingAs($user)
+                    ->call(
+                        'GET',
+                        '/project/like/'.$project->id
+                    );
+        $this->dontSeeInDatabase('projects_likes', ['user_id' => $user->id, 'project_id' => $project->id]);
+    }
+
+    /**
+     * Test for viewing a project on project
+     *
+     * @return void
+     */
+
+    public function testViewProject()
+    {
+        $user = factory(Pibbble\User::class)->create();
+        $project = factory(Pibbble\Project::class)->create();
+
+        $this->visit('/project/view/'.$project->id)
+            ->seeInDatabase('projects', ['id' => 1,'views' => 1])
+            ->visit('/project/view/'.$project->id)
+            ->seeInDatabase('projects', ['id' => 1,'views' => 2]);
     }
 }
