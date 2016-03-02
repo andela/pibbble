558149c046e4a9db71b6e0d2858bbe11f9ebe622
andela-aonyango <andela-aonyango@users.noreply.github.com>
2016-03-02 10:11:32 +0300

    [Feature #111860601] Developer meetup (Add a link for pending meetups)
    Added a link in the dropdown menu for pending meetups so that users
    can access them.

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -37,6 +37,7 @@
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">Meetups<span class="caret"></span></a>
               <ul class="dropdown-menu" role="menu">
                 <li><a href="#">All</a></li>
+                <li><a href="/meetup/pending">Pending</a></li>
                 <li><a href="{{ url('/meetup/new') }}">Host a Meetup</a></li>
               </ul>
             </li>
