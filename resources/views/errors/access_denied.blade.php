602d22bd6dd44abbebe042bbf0bd1dd65343a9d6
andela-aonyango <andrew.onyango@andela.com>
2016-03-07 22:14:27 +0300

    [Feature #113260979] Add view to redirect unauthorised users to

---
+++ resources/views/errors/access_denied.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('title', 'Access Denied')
+@section('content')
+
+    <div class="error404" style="min-height: 160px">
+        <h2 class="error">Access Denied</h2>
+        <h3 class="error-message">
+            You are not authorised to access this page.
+        </h3>
+    </div>
+
+@endsection
