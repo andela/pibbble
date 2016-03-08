6d035c6bb091ed93a3d0b647a04ecc86f576aa32
andela-aonyango <andrew.onyango@andela.com>
2016-03-08 11:24:44 +0300

    [Feature #113260979] Add 'approved' column to meetups table
    Redid the migrations to add an approved column to the meetups table.

---
+++ database/migrations/2016_03_08_081324_add_approved_column_meetups.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddApprovedColumnMeetups extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meetups', function (Blueprint $table) {
+            $table->boolean('approved');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('meetups', function (Blueprint $table) {
+            $table->dropColumn('approved');
+        });
+    }
+}
